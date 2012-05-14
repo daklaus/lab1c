8d1fc4c0b9c527a0553682559ca4d34a1ab179a7
Klaus Krapfenbauer <klaus.krapfenbauer@gmx.com>
2012-05-14 19:10:49 +0200

    Added initialized project for app1

---
+++ 10/app1/src/at/ac/tuwien/secsyseng/sqlinjection/Main.java
@@ -0,0 +1,54 @@
+package at.ac.tuwien.secsyseng.sqlinjection;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import at.ac.tuwien.secsyseng.sqlinjection.dao.ConnectionFactory;
+
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+		try {
+			initializeDb();
+			
+			test(); // Later, remove this line and start GUI here
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	private static void test() throws SQLException {
+		Connection c = ConnectionFactory.getConnection();
+
+		Statement st = c.createStatement();
+		ResultSet rs = st.executeQuery("select * from account;");
+
+		while (rs.next()) {
+			System.out.println(rs.getInt("id") + "|" + rs.getString("name")
+					+ "|" + rs.getDouble("amount"));
+		}
+	}
+
+	private static void initializeDb() throws SQLException {
+		Connection c = ConnectionFactory.getConnection();
+		Statement st = c.createStatement();
+		
+		// Create tables
+		st.executeUpdate("create table account (" + "id integer primary key,"
+				+ "name varchar(255) not null," + "amount numeric not null);");
+		
+		// Insert into account
+		st.executeUpdate("insert into account values (0, 'christian', 10000);");
+		st.executeUpdate("insert into account values (1, 'aljosha', 11000);");
+		st.executeUpdate("insert into account values (2, 'johannes', 9000);");
+		st.executeUpdate("insert into account values (3, 'klaus', 8000);");
+	}
+
+}
