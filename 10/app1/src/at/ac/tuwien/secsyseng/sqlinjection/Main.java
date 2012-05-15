a228bd3a3c0d4fea23b230ff94ccb85dc4a69322
Johannes LUEF <Johannes.Luef@student.tuwien.ac.at>
2012-05-15 23:36:32 +0200

    Var3 implemented: SQL Injection in Insert Query

--- 10/app1/src/at/ac/tuwien/secsyseng/sqlinjection/Main.java
+++ 10/app1/src/at/ac/tuwien/secsyseng/sqlinjection/Main.java
@@ -17,21 +17,20 @@ public class Main {
 
 		try {
 			initializeDb();
-			
+
 			test(); // Later, remove this line and start GUI here
-			
+
 			MainFrame.launch();
 
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
-	
 
 	private static void test() throws SQLException {
 		Connection c = ConnectionFactory.getConnection();
 		Statement st = c.createStatement();
-		
+
 		// account test
 		System.out.println("============");
 		System.out.println("account test");
@@ -39,10 +38,11 @@ public class Main {
 		ResultSet rs = st.executeQuery("select * from account;");
 		while (rs.next()) {
 			System.out.println(rs.getInt("id") + "|" + rs.getString("user")
-					+ "|" + rs.getString("password")+ "|" + rs.getDouble("amount"));
+					+ "|" + rs.getString("password") + "|"
+					+ rs.getDouble("amount"));
 		}
 		System.out.println("\n");
-		
+
 		// account test
 		System.out.println("=============");
 		System.out.println("var1data test");
@@ -50,34 +50,63 @@ public class Main {
 		rs = st.executeQuery("select * from var1data;");
 		while (rs.next()) {
 			System.out.println(rs.getInt("id") + "|" + rs.getString("product")
-					+ "|" + rs.getString("location") + "|" + rs.getDouble("price"));
+					+ "|" + rs.getString("location") + "|"
+					+ rs.getDouble("price"));
 		}
+
+		// group test
+		System.out.println("=============");
+		System.out.println("var3data test");
+		System.out.println("=============");
+
+		rs = st.executeQuery("select * from var3data;");
+
+		while (rs.next()) {
+			System.out.println(rs.getInt("id") + "|" + rs.getString("name"));
+		}
+
 	}
-	
 
 	private static void initializeDb() throws SQLException {
 		Connection c = ConnectionFactory.getConnection();
 		Statement st = c.createStatement();
-		
+
 		// Create table account
 		st.executeUpdate("create table account (" + "id integer primary key,"
-				+ "user varchar(255) not null," + "password varchar(255) not null," + "amount numeric not null);");
-		
+				+ "user varchar(255) not null,"
+				+ "password varchar(255) not null,"
+				+ "amount numeric not null);");
+
 		// Create table var1data
 		st.executeUpdate("create table var1data (" + "id integer primary key,"
-				+ "product varchar(255) not null," + "location varchar(255) not null," + "price numeric not null);");
-		
+				+ "product varchar(255) not null,"
+				+ "location varchar(255) not null,"
+				+ "price numeric not null);");
+
+		// Create table var3data
+		st.executeUpdate("create table var3data (" + "id integer primary key,"
+				+ "name varchar(255) not null );");
+
 		// Insert into account
 		st.executeUpdate("insert into account values (0, 'christian', 'pass1', 10000);");
 		st.executeUpdate("insert into account values (1, 'aljosha', 'pass2', 11000);");
 		st.executeUpdate("insert into account values (2, 'johannes', 'pass3', 9000);");
 		st.executeUpdate("insert into account values (3, 'klaus', 'pass4', 8000);");
 		st.executeUpdate("insert into account values (4, 'wong', 'dong', 666);");
-		
+
 		// Insert into var1data
 		st.executeUpdate("insert into var1data values (0, 'Agile Custom Interceptor 7 String', 'America', 1325);");
 		st.executeUpdate("insert into var1data values (1, 'Wong Dong', 'Austria - InetSec', 25);");
 		st.executeUpdate("insert into var1data values (2, 'Matt Damon', 'America', 1);");
 		st.executeUpdate("insert into var1data values (3, 'Fiat Stilo', 'Austria', 7000);");
+
+		// Inserts into var3data
+		st.executeUpdate("insert into var3data values (1, 'Gruppe 1');");
+		st.executeUpdate("insert into var3data values (2, 'Gruppe 2');");
+		st.executeUpdate("insert into var3data values (3, 'Gruppe 3');");
+		st.executeUpdate("insert into var3data values (4, 'Gruppe 4');");
+		st.executeUpdate("insert into var3data values (5, 'Gruppe 5');");
+		// st.executeUpdate("insert into var3data values (6, 'Gruppe 6'); drop table var3data;");
+
 	}
 }
